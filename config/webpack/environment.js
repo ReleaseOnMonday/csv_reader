401b6c73dec3504a4958c5f9d346585cc4fefb7d
Ayush <laadayush@gmail.com>
2021-07-30 01:58:47 +0530

    Add angular support

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
-
+environment.loaders.append('html', {
+	test: /\.html$/,
+	use: [{
+		loader: 'html-loader',
+		options: {
+			minimize: true,
+			removeAttributeQuotes: false,
+			caseSensitive: true,
+			customAttrSurround: [
+					[/#/, /(?:)/],
+					[/\*/, /(?:)/],
+					[/\[?\(?/, /(?:)/]
+			],
+			customAttrAssign: [/\)?\]?=/]
+		}
+	}]
+})
 module.exports = environment
